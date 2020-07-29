80353ceb6b535eaeff69bd3bb68014ee15186c2e
JoshSunny <44406978+JoshSunny@users.noreply.github.com>
2020-07-29 23:57:19 +0800

    Add files via upload

---
+++ View/mainwindow.h
@@ -0,0 +1,44 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include "../Common/Music.h"
+
+#include <QMainWindow>
+#include <QWidget>
+#include <QMovie>
+#include <QTimer>
+#include <QLabel>
+#include <QMouseEvent>
+#include <QPushButton>
+#include <QKeyEvent>
+#include <QPaintEvent>
+#include <QPainter>
+#include <QList>
+#include <QtGlobal>
+#include <QMessageBox>
+#include <QXmlStreamReader>
+#include <QtDebug>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+    AudioPlayer * m_audioPlayer;
+
+protected:
+    void paintEvent(QPaintEvent *);
+    //void mousePressEvent(QMouseEvent *);//鼠标点击触发事件
+
+private:
+    Ui::MainWindow *ui;
+};
+
+#endif // MAINWINDOW_H
